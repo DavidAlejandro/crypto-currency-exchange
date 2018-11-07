74989fda13358e60c54ccd6e8eff005eb0177405
David Porcel <davidporcel91@gmail.com>
2018-11-07 06:00:50 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+import Container from 'react-bootstrap/lib/Container';
+import OrderBook from './containers/OrderBook';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './store/reducers';
+
 import './App.css';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 class App extends Component {
+
+
+
+  componentDidMount(){
+
+    // let bookRequest = JSON.stringify({ 
+    //   event: 'subscribe', 
+    //   channel: 'book', 
+    //   symbol: 'tBTCUSD' 
+    // })
+      
+    // const ws = new Sockette('wss://api.bitfinex.com/ws/2', {
+    //   timeout: 5e3,
+    //   maxAttempts: 10,
+    //   onopen: onConnectionEstablished,
+    //   onmessage: onMessageRecieved,
+    //   onreconnect: e => console.log('Reconnecting...', e),
+    //   onmaximum: e => console.log('Stop Attempting!', e),
+    //   onclose: e => onConnectionClosed,
+    //   onerror: e => console.log('Error:', e)
+    // });
+    
+    // function onConnectionEstablished(e){
+    //   console.log('connected');
+    //   console.log(e);
+    //   ws.send(bookRequest);
+    // }
+
+    // function onConnectionClosed(e){
+    //   console.log('closed');
+    // }
+
+    // function onMessageRecieved(e){
+    //     console.log('got a new message!');
+    //     console.log(e);
+    // }
+
+    // // ws.close(); // graceful shutdown
+
+    // // Reconnect 10s later
+    // setTimeout(ws.reconnect, 10e3);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <div className="App">
+          <OrderBook/>
+        </div>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
